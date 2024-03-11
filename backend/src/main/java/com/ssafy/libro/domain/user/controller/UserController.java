dde997283a279124822cd26675ac38525430cead
DevYJKwon <dydwo2324@gmail.com>
2024-03-11 15:55:50 +0900

    feat: add OAuth2.0 user join logic

--- backend/src/main/java/com/ssafy/libro/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/libro/domain/user/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.ssafy.libro.domain.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.ssafy.libro.domain.user.dto.UserJoinRequestDto;
+import com.ssafy.libro.domain.user.service.UserService;
+import com.ssafy.libro.global.util.entity.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/login")
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 @RestController
 public class UserController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello!!";
+    private final UserService userService;
+    private final Response response;
+    @PostMapping("/join")
+    public ResponseEntity<?> joinUser(@RequestBody UserJoinRequestDto requestDto) {
+        return userService.joinUser(requestDto);
     }
 }
