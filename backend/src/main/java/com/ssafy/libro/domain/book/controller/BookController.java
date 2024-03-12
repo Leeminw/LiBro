ff94944f380a7a9973bd568b53971a6b89740ac2
조시훈 <kepler452k@naver.com>
2024-03-12 10:12:35 +0900

    feat: apply ResponseData

--- backend/src/main/java/com/ssafy/libro/domain/book/controller/BookController.java
+++ backend/src/main/java/com/ssafy/libro/domain/book/controller/BookController.java
@@ -4,6 +4,7 @@ import com.ssafy.libro.domain.book.dto.BookCreateRequestDto;
 import com.ssafy.libro.domain.book.dto.BookDetailResponseDto;
 import com.ssafy.libro.domain.book.dto.BookUpdateRequestDto;
 import com.ssafy.libro.domain.book.service.BookServiceImpl;
+import com.ssafy.libro.global.common.ResponseData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,31 +20,31 @@ public class BookController {
     @PostMapping("/api/v1/book")
     public ResponseEntity<?> createBook(@RequestBody BookCreateRequestDto requestDto) {
         BookDetailResponseDto responseDto = bookService.createBook(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseData.success(responseDto));
     }
 
     @PutMapping("/api/v1/book")
     public ResponseEntity<?> updateBook(@RequestBody BookUpdateRequestDto requestDto) {
         BookDetailResponseDto responseDto = bookService.updateBook(requestDto);
-        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success(responseDto));
     }
 
     @DeleteMapping("/api/v1/book/{id}")
     public ResponseEntity<?> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ResponseData.success("Delete Success"));
     }
 
     @GetMapping("/api/v1/book/{id}")
     public ResponseEntity<?> getBook(@PathVariable Long id) {
         BookDetailResponseDto responseDto = bookService.getBook(id);
-        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success(responseDto));
     }
 
     @GetMapping("/api/v1/books")
     public ResponseEntity<?> getBooks() {
         List<BookDetailResponseDto> responseDto = bookService.getBooks();
-        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success(responseDto));
     }
 
 }
