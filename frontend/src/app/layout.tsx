9d39fa6599aa48627c8e426f28ebddff75c42dec
조승우 <kjsw12@naver.com>
2024-03-15 10:01:09 +0900

    feature: 토스트 관련 사용가능하도록 설정, 모바일 환경에 맞게 레이아웃 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import BottomNavigation from "@/components/BottomNavigation";
+import {Toaster} from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="ko">
+      <body className="App min-h-screen max-w-md relative bg-gray-100 mx-auto overscroll-y-none touch-none">
+          {children}
+          <BottomNavigation/>
+            <Toaster/>
+      </body>
+      </html>
   );
 }
