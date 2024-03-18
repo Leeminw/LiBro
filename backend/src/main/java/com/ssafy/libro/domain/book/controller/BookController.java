13c9a3ef836f7a983e7b857f18df75c1374b9720
leeminwoo <asdfn0@naver.com>
2024-03-18 14:05:37 +0900

    feat: add book search by isbn,title,author

--- backend/src/main/java/com/ssafy/libro/domain/book/controller/BookController.java
+++ backend/src/main/java/com/ssafy/libro/domain/book/controller/BookController.java
@@ -6,12 +6,16 @@ import com.ssafy.libro.domain.book.dto.BookUpdateRequestDto;
 import com.ssafy.libro.domain.book.service.BookServiceImpl;
 import com.ssafy.libro.global.common.ResponseData;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class BookController {
@@ -46,5 +50,31 @@ public class BookController {
         List<BookDetailResponseDto> responseDto = bookService.getBooks();
         return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success(responseDto));
     }
+
+    @GetMapping("/api/v1/book/search")
+    public ResponseEntity<?> getBooksByTitle(
+            @RequestParam String key, @RequestParam String word, @RequestParam Integer page, @RequestParam Integer size){
+        List<BookDetailResponseDto> responseDto = null;
+        log.debug("controller page : {} , size : {}", page, size);
+        if("isbn".equals(key)){
+            // isbn 조회
+            responseDto = bookService.getBooksByIsbn(word);
+        }
+        else if("title".equals(key)){
+            // title 조회
+            responseDto = bookService.getBooksByTitle(word, PageRequest.of(page,size));
+        }
+        else if("author".equals(key)){
+            // 작가조회
+            responseDto = bookService.getBooksByAuthor(word, PageRequest.of(page,size));
+        }
+        else{
+            //키워드가 없는 경우
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseData.error("잘못된 요청입니다."));
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success(responseDto));
+    }
+
 
 }
