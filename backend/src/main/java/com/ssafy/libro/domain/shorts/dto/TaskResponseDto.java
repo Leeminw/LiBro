403156f30f1adef040f0e04589abfcc83c107867
조시훈 <kepler452k@naver.com>
2024-03-21 14:28:47 +0900

    feat: add dto for stable diffusion tasks

---
+++ backend/src/main/java/com/ssafy/libro/domain/shorts/dto/TaskResponseDto.java
@@ -0,0 +1,41 @@
+package com.ssafy.libro.domain.shorts.dto;
+
+import com.ssafy.libro.domain.shorts.entity.Task;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskResponseDto {
+    private Long id;
+    private String isbn;
+    private String title;
+    private Boolean status;
+
+    private String summary;
+    private String korPrompt;
+    private String engPrompt;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    public TaskResponseDto(Task entity) {
+        this.id = entity.getId();
+        this.isbn = entity.getIsbn();
+        this.title = entity.getTitle();
+        this.status = entity.getStatus();
+
+        this.summary = entity.getSummary();
+        this.korPrompt = entity.getKorPrompt();
+        this.engPrompt = entity.getEngPrompt();
+
+        this.createdDate = entity.getCreatedDate();
+        this.updatedDate = entity.getUpdatedDate();
+    }
+}
