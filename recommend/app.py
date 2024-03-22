f56112a3922f386d1b3d82f40ab6346d6ac5507d
leeminwoo <asdfn0@naver.com>
2024-03-22 16:51:15 +0900

    feat: add random book recommend

--- recommend/app.py
+++ recommend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, make_response
+# -- coding: utf-8 --
+from flask import Flask, make_response, Response, json
 import os
 from dotenv import load_dotenv
 from flask import jsonify
@@ -6,36 +7,68 @@ from http import HTTPStatus
 import random
 from config.db import get_db
 from sqlalchemy import create_engine
-
-
+from sqlalchemy.sql import text
+import datetime
 app = Flask(__name__)
 
 # DB 연결
 engine = get_db()
-
+app.config['JSON_AS_ASCII'] = False
 
 def response_entity(data = "", status = 200) :
     success = status//100 <= 2
-    return jsonify(
+    payload = json.dumps(
         {
             "status" : "success" if success else "error",
             "message" : "The request has been processed successfully." if  success 
             else "An unexpected error occurred while processing the request." ,
             "data" : data 
-        }
+        },
+        ensure_ascii=False,
+        indent=4
     )
 
+    response = Response(payload, content_type="application/json; charset=utf-8")
+    return response
+
 @app.route('/')
-def home() :
+def random_book() :
     with engine.connect() as conn : 
-        result = conn.execute(
-            
+        
+        statement = text(
+            """
+            SELECT count(*) 
+            FROM book
+            """
+            )
+
+        result = conn.execute(statement)
+        size = result.fetchone()[0]
+        choosed = tuple(random.sample(range(1, size + 1), 10))
+
+
+        statement = text(
+            '''
+            SELECT *
+            FROM book
+            WHERE id in {}
+            '''.format(choosed)
         )
 
-        for r in result :
-            print(r)
-
-    return response_entity("hello", HTTPStatus.INTERNAL_SERVER_ERROR )
+        result = conn.execute(statement)
+        response = []
+        keys = result.keys()
+        for r in result : 
+            tmp = dict()
+            for key in keys:
+                tmp[key] = r._get_by_key_impl_mapping(key)
+                if isinstance(tmp[key], datetime.datetime):
+                    tmp[key] = tmp[key].strftime("%Y-%m-%d %H:%M:%S")
+                    print(type(tmp[key]))
+            
+            response.append(tmp)
+    
+    return response_entity(response, HTTPStatus.OK)
 
 
 @app.route('/flask/api/v1/recommend')
