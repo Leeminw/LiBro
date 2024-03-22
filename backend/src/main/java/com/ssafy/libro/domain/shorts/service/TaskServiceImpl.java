0f79e159423a3f14b766ec2480f8a7536177fbe7
조시훈 <kepler452k@naver.com>
2024-03-22 17:50:12 +0900

    feat: add scheduling functions to check and update AI image creating tasks

--- backend/src/main/java/com/ssafy/libro/domain/shorts/service/TaskServiceImpl.java
+++ backend/src/main/java/com/ssafy/libro/domain/shorts/service/TaskServiceImpl.java
@@ -1,5 +1,10 @@
 package com.ssafy.libro.domain.shorts.service;
 
+import com.ssafy.libro.domain.book.entity.Book;
+import com.ssafy.libro.domain.book.exception.BookNotFoundException;
+import com.ssafy.libro.domain.book.repository.BookRepository;
+import com.ssafy.libro.domain.shorts.dto.PromptRequestDto;
+import com.ssafy.libro.domain.shorts.dto.PromptResponseDto;
 import com.ssafy.libro.domain.shorts.dto.TaskRequestDto;
 import com.ssafy.libro.domain.shorts.dto.TaskResponseDto;
 import com.ssafy.libro.domain.shorts.entity.Task;
@@ -7,19 +12,26 @@ import com.ssafy.libro.domain.shorts.exception.TaskNotFoundException;
 import com.ssafy.libro.domain.shorts.repository.TaskJpaRepository;
 import com.ssafy.libro.domain.shorts.repository.TaskRedisRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.security.SecureRandom;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
+    private final BookRepository bookRepository;
     private final TaskJpaRepository taskJpaRepository;
     private final TaskRedisRepository taskRedisRepository;
+    private final PromptServiceImpl promptService;
 
     @Override
     public TaskResponseDto createTask(TaskRequestDto requestDto) {
@@ -96,4 +108,52 @@ public class TaskServiceImpl implements TaskService {
         return tasks.stream().map(TaskResponseDto::new).collect(Collectors.toList());
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private final SecureRandom secureRandom = new SecureRandom();
+    private final int minWaitTime = 100; // Minimum wait time in milliseconds
+    private final int maxWaitTime = 500; // Maximum wait time in milliseconds
+
+    @Scheduled(cron = "0 */1 * * * *", zone = "Asia/Seoul")
+    public void updateTasks() throws IOException {
+        log.info("Task Update Schedule Processing...");
+        List<Book> books = bookRepository.findAllByShortsUrlIsNull().orElseThrow(
+                () -> new BookNotFoundException(""));
+
+        int count = 0;
+        for (Book book : books) {
+            if (taskJpaRepository.existsByIsbn(book.getIsbn()))
+                continue;
+
+            PromptResponseDto prompts = promptService.convertText2Prompt(
+                    PromptRequestDto.builder()
+                            .title(book.getTitle())
+                            .summary(book.getSummary())
+                            .build()
+            );
+
+            TaskRequestDto taskRequestDto = TaskRequestDto.builder()
+                    .isbn(book.getIsbn())
+                    .title(book.getTitle())
+                    .status(false)
+                    .summary(book.getSummary())
+                    .korPrompt(prompts.getKorPrompt())
+                    .engPrompt(prompts.getEngPrompt())
+                    .build();
+
+
+            Task task = taskJpaRepository.save(taskRequestDto.toEntity());
+            log.info("Task Saved Success: " + task.getTitle());
+
+            int randomWaitTime = secureRandom.nextInt(maxWaitTime - minWaitTime + 1) + minWaitTime;
+            try {
+                log.debug("Waiting for " + randomWaitTime + " milliseconds...");
+                Thread.sleep(randomWaitTime);
+            } catch (InterruptedException e) {
+                log.error("Thread interrupted while waiting.");
+                Thread.currentThread().interrupt(); // Restore interrupted status
+            }
+            if (count++ > 50) break;
+        }
+        log.info("Task Update Schedule Complete!!!");
+    }
 }
