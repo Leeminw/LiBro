e3f1d82e19a8f3cb58675cdfc140d9450870cfde
조승우 <kjsw12@naver.com>
2024-03-24 02:26:05 +0900

    feature: 컴포넌트 수정 및 추가

--- frontend/src/components/components/admin/UserProfileProvider.tsx
+++ frontend/src/components/components/admin/UserProfileProvider.tsx
@@ -1,20 +1,33 @@
+'use client';
+
 import UserProfile from "@/components/components/admin/UserProfile";
+import {useQuery} from "@tanstack/react-query"; // 수정된 import 구문
+import {useParams} from "next/navigation";
+import {getClubMemberList} from "@/lib/club";
+import {Skeleton} from "@/components/ui/skeleton";
 
-interface MemberAdminProps {
-    userProfiles: UserProfile[];
-}
+export default function UserProfileProvider() {
+    const params = useParams();
+    const clubId = parseInt(params.id);
 
-export default function UserProfileProvider(props : MemberAdminProps) {
-    const {userProfiles} = props
-    return (
-        userProfiles.map(user  => (
-                <UserProfile
-                    key={user.userNo}
-                    profileImageUrl={user.profileImageUrl}
-                    userName={user.userName}
-                    userNo={user.userNo}
-                    date={user.date}
-                />
-            ))
-    )
+    const {data, isError, isSuccess, isLoading, refetch} = useQuery({
+        queryKey: ['memberList', clubId],
+        queryFn: () => getClubMemberList(clubId) // context 매개변수를 제거
+    });
+
+    if (isLoading) return <Skeleton> </Skeleton>;
+    if (isError) return <>Error</>;
+
+    return isSuccess && (
+        data.map(user => (
+            <UserProfile
+                key={user.userId}
+                profile={user.profile}
+                name={user.name}
+                userId={user.userId}
+                role={user.role}
+                createdDate={user.createdDate}
+            />
+        ))
+    );
 }
---
+++ frontend/src/components/components/club/ClubListCard.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import React, {useEffect, useState} from 'react';
+import {Card, CardContent} from '@/components/ui/card'; // your-component-library에 실제로 사용하는 라이브러리 명을 입력해야 합니다.
+import {CalendarIcon, Search} from "lucide-react"
+import {Input} from "@/components/ui/input";
+import {Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {useInView} from "react-intersection-observer";
+import {useInfiniteQuery} from "@tanstack/react-query";
+import {getClubList} from "@/lib/club";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {Skeleton} from "@/components/ui/skeleton";
+import {dateView} from "@/lib/dayjs";
+import Link from "next/link";
+
+const FindClubCard: React.FC = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [sortOrder, setSortOrder] = useState('latest'); // 초기 정렬 순서를 설정합니다. 여기서는 최신순으로 초기화합니다.
+
+    const {
+        data: clubs,
+        isLoading,
+        isError,
+        hasNextPage,
+        fetchNextPage,
+        isFetchingNextPage,
+        refetch,
+    } = useInfiniteQuery({
+        queryKey: ['clubList'],
+        queryFn: ({pageParam}) => getClubList({
+            sortOrder: sortOrder,
+            keyword: searchTerm,
+            clubId: pageParam,
+        }),
+        getNextPageParam: (lastPage, allPages) => {
+
+            const nextCursor = lastPage.content.length === 0 ? undefined : lastPage.content[lastPage.content.length - 1].clubId;
+            console.log(nextCursor)
+            return nextCursor;
+        },
+    })
+
+    const handleSearchKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setSearchTerm(event.currentTarget.value);
+        }
+    };
+
+    const handleSortChange = (event) => {
+        setSortOrder(event);
+    };
+
+    const {ref, inView} = useInView();
+
+    useEffect(() => {
+        if (inView && hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+        }
+    }, [inView]);
+
+    useEffect(() => {
+        refetch();
+    }, [sortOrder, searchTerm]);
+
+    if (isLoading) {
+        return <Skeleton></Skeleton>;
+    }
+
+    if (isError) {
+        return <div>Error</div>;
+    }
+
+    console.log(inView)
+
+
+    return (
+        <>
+            <div className="flex items-center justify-between w-2/3">
+                <Select onValueChange={handleSortChange}>
+                    <SelectTrigger className={""}>
+                        <SelectValue placeholder="정렬기준"/>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="latest">최신</SelectItem>
+                            <SelectItem value="oldest">가장 오래된</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+
+            <div className="w-2/3">
+                <div className="relative ">
+                    <Search className="absolute left-2 top-3 h-4 w-4 text-muted-foreground"/>
+                    <Input placeholder="Search" className="pl-8" onKeyDown={handleSearchKeyDown}/>
+                </div>
+            </div>
+
+            <Card>
+                <ScrollArea className="flex flex-col max-w-md mx-auto bg-white h-[calc(100vh-140px)]">
+                    <div>
+                        {clubs.pages.flatMap(t => t.content).map((club) => (
+                            <Card key={club.clubId} className="pt-6 bg-white rounded-lg shadow-md mt-2 mb-2">
+                                <CardContent>
+                                    <Link href={`/club/${club.clubId}`}
+                                          className="text-lg font-bold">{club.clubName}</Link>
+                                    <div className="flex items-center space-x-2 mb-4">
+                                        <CalendarIcon className="w-4 h-4 text-gray-500"/>
+                                        <span className="text-sm text-gray-500">{dateView(club.createdDate)}</span>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        {club.clubOwnerName}
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </div>
+
+                    <div ref={ref}></div>
+                </ScrollArea>
+
+                <div className="mb-16"/>
+            </Card>
+        </>
+    );
+};
+
+export default FindClubCard;
