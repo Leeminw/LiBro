a8e954ff316c69c589e210f71badab77d82e4a7d
조승우 <kjsw12@naver.com>
2024-03-24 02:21:51 +0900

    feature : 게시판(Board) 수정 로직 추가 및 수정

--- backend/src/main/java/com/ssafy/libro/domain/board/dto/BoardCreateRequestDto.java
+++ backend/src/main/java/com/ssafy/libro/domain/board/dto/BoardCreateRequestDto.java
@@ -1,5 +1,7 @@
 package com.ssafy.libro.domain.board.dto;
 
+import com.ssafy.libro.domain.board.entity.Board;
+import com.ssafy.libro.domain.club.entity.Club;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,4 +10,11 @@ import lombok.Getter;
 public class BoardCreateRequestDto {
     private final Long clubId;
     private final String name;
+
+    public Board toEntity(Club club){
+        return Board.builder()
+                .club(club)
+                .name(name)
+                .build();
+    }
 }
--- backend/src/main/java/com/ssafy/libro/domain/board/service/BoardService.java
+++ backend/src/main/java/com/ssafy/libro/domain/board/service/BoardService.java
@@ -2,14 +2,15 @@ package com.ssafy.libro.domain.board.service;
 
 import com.ssafy.libro.domain.board.dto.BoardCreateRequestDto;
 import com.ssafy.libro.domain.board.dto.BoardResponseDto;
+import com.ssafy.libro.domain.board.dto.BoardUpdateRequestDto;
 
 import java.util.List;
 
 public interface BoardService {
     List<BoardResponseDto> getBoardList(Long groupId);
-    BoardResponseDto getBoard();
 
-    void createBoard(BoardCreateRequestDto dto);
+    Long createBoard(BoardCreateRequestDto dto);
+    void updateBoard(BoardUpdateRequestDto dto);
 
     void deleteBoard(Long boardId);
 }
