3538d59f8110e5ae1f6c83456717d988d3907b9a
조승우 <kjsw12@naver.com>
2024-03-24 18:16:32 +0900

    feature: axios 코드 추가

---
+++ frontend/src/lib/axios-login.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "http://localhost:8080",
+});
+const LoginApi = {
+    loadUser: async (token: string) => {
+        try {
+            console.log("token", token);
+            const response = await apiClient.get("/api/user/load", {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    logoutUser: async (token: string) => {
+        try {
+            const response = await apiClient.get("/api/token/logout", {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    test: async (token: string) => {
+        try {
+            const response = await apiClient.get("/api/user/test", {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+};
+
+export {LoginApi};
---
+++ frontend/src/lib/login-state.ts
@@ -0,0 +1,56 @@
+import {create} from "zustand";
+import {createJSONStorage, persist, PersistStorage} from "zustand/middleware";
+import {LoginApi} from "./axios-login";
+
+interface UserInfoType {
+    email: string;
+    name: string;
+    profile: string;
+    nickname: string;
+}
+
+interface UserState {
+    userInfo: UserInfoType;
+    getUserInfo: () => UserInfoType;
+    setUserInfo: (userInfo: UserInfoType) => void;
+    deleteUserInfo: () => void;
+    isLogin: boolean;
+}
+
+const defaultState: UserInfoType = {
+    email: "",
+    name: "",
+    profile: "",
+    nickname: "",
+};
+
+const storage = createJSONStorage(() => localStorage) as PersistStorage<UserState>;
+
+const useUserState = create(
+    persist<UserState>(
+        (set, get) => ({
+            userInfo: defaultState,
+            isLogin: typeof window !== "undefined" ? !!localStorage.getItem("accessToken") : false,
+            getUserInfo: () => {
+                return get().userInfo;
+            },
+            setUserInfo: (userInfo: UserInfoType) => {
+                set({userInfo});
+            },
+            deleteUserInfo: () => {
+                const refreshToken = localStorage.getItem("refreshToken");
+                if (refreshToken) LoginApi.logoutUser(refreshToken);
+                localStorage.removeItem("id");
+                localStorage.removeItem("accessToken");
+                localStorage.removeItem("refreshToken");
+                set({userInfo: defaultState});
+            },
+        }),
+        {
+            name: "userInfoStorage",
+            storage,
+        }
+    )
+);
+
+export default useUserState;
