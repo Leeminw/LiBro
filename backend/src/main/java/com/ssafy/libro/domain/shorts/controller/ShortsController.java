1da999e7456a4cea57b88239b122c554e9dc1d0f
조시훈 <kepler452k@naver.com>
2024-03-26 18:02:06 +0900

    feat: add api creating shorts but not finished

--- backend/src/main/java/com/ssafy/libro/domain/shorts/controller/ShortsController.java
+++ backend/src/main/java/com/ssafy/libro/domain/shorts/controller/ShortsController.java
@@ -1,16 +1,28 @@
 package com.ssafy.libro.domain.shorts.controller;
 
+import com.ssafy.libro.domain.book.dto.BookDetailResponseDto;
 import com.ssafy.libro.domain.book.service.BookService;
 import com.ssafy.libro.domain.book.service.BookServiceImpl;
+import com.ssafy.libro.domain.shorts.dto.PromptRequestDto;
+import com.ssafy.libro.domain.shorts.dto.PromptResponseDto;
+import com.ssafy.libro.domain.shorts.dto.ShortsRequestDto;
+import com.ssafy.libro.domain.shorts.dto.ShortsResponseDto;
 import com.ssafy.libro.domain.shorts.service.PromptServiceImpl;
 import com.ssafy.libro.domain.shorts.service.ShortsService;
 import com.ssafy.libro.domain.shorts.service.ShortsServiceImpl;
+import com.ssafy.libro.domain.shorts.service.TaskServiceImpl;
 import com.ssafy.libro.global.common.ResponseData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -18,17 +30,36 @@ import org.springframework.web.bind.annotation.*;
 public class ShortsController {
 
     private final BookServiceImpl bookService;
+    private final TaskServiceImpl taskService;
     private final PromptServiceImpl promptService;
     private final ShortsServiceImpl shortsService;
 
+    @GetMapping("/api/v1/shorts")
+    public ResponseEntity<?> getShortsByBookId(@RequestParam("book-id") Long bookId) throws IOException {
+        BookDetailResponseDto bookResponseDto = bookService.getBook(bookId);
+        PromptRequestDto promptRequestDto = PromptRequestDto.builder()
+                .title(bookResponseDto.getTitle())
+                .content(bookResponseDto.getSummary())
+                .build();
+        PromptResponseDto promptResponseDto = promptService.translateText2Prompt(promptRequestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success(bookResponseDto));
+    }
+
+    @PostMapping("/api/v1/shorts/create")
+    public ResponseEntity<?> createShorts(@RequestBody ShortsRequestDto requestDto) throws IOException {
+        ShortsResponseDto responseDto = shortsService.createShorts(requestDto);
+        String filename = responseDto.getResource().getFilename();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(ResponseData.success(responseDto));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @GetMapping("/api/v1/shorts/create/test")
     public ResponseEntity<?> createShortsTest() {
         shortsService.createShorts();
-        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("쇼츠영상 S3 주소가 들어갈 예정"));
-    }
-
-    @GetMapping("/api/v1/shorts/create")
-    public ResponseEntity<?> createShorts(@RequestParam("book-id") Long bookId) {
         return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success("쇼츠영상 S3 주소가 들어갈 예정"));
     }
 
