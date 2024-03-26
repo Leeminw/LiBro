d944b0db7865d055aa19d8170fff24fbdd0e6dbc
조시훈 <kepler452k@naver.com>
2024-03-27 03:43:38 +0900

    refactor: remove previous code

--- backend/src/main/java/com/ssafy/libro/domain/shorts/service/ShortsService.java
+++ backend/src/main/java/com/ssafy/libro/domain/shorts/service/ShortsService.java
@@ -1,7 +1,5 @@
 package com.ssafy.libro.domain.shorts.service;
 
-import com.ssafy.libro.domain.book.dto.BookDetailResponseDto;
-import com.ssafy.libro.domain.shorts.dto.PromptRequestDto;
 import com.ssafy.libro.domain.shorts.dto.ShortsRequestDto;
 import com.ssafy.libro.domain.shorts.dto.ShortsResponseDto;
 
@@ -9,9 +7,6 @@ import java.io.IOException;
 
 public interface ShortsService {
     ShortsResponseDto createShorts(ShortsRequestDto requestDto) throws IOException;
-    BookDetailResponseDto getShortsByBookId(Long bookId) throws IOException;
+    ShortsResponseDto getShortsByBookId(Long bookId) throws IOException;
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    void createImages(PromptRequestDto requestDto) throws IOException;
-    void createShorts();
 }
