af758915b500f6c2ab61215f8feabc3db89bdbae
leeminwoo <asdfn0@naver.com>
2024-03-27 10:48:40 +0900

    fix : fix login error

--- backend/src/main/java/com/ssafy/libro/domain/article/entity/Article.java
+++ backend/src/main/java/com/ssafy/libro/domain/article/entity/Article.java
@@ -51,9 +51,8 @@ public class Article {
     @JoinColumn(name = "board_id")
     private Board board;
 
-    @Builder.Default
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> comments = new ArrayList<>();
+    private List<Comment> comments;
 
     public Article(ArticleUpdateRequestDto dto){
         this.title = dto.getTitle();
