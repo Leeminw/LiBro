62ba337d186718900b06e8826efd8c997dc1e0aa
leeminwoo <asdfn0@naver.com>
2024-03-29 12:22:24 +0900

    feat: add recommend random

--- recommend/service/service.py
+++ recommend/service/service.py
@@ -17,17 +17,17 @@ def get_random_book_list(size : int ) -> List[dict] :
 def get_recommend_book_list(user_id : int, size : int ) -> List[dict] :
     engine = get_db() 
     # 책 갯수 찾기
-    n_books = get_user_book_count_distinct(engine)
+    # n_books = get_user_book_count_distinct(engine)
     # print("최대 도서 갯수 ", n_books)
-    
-    print("load model")
+    total_count = get_total_book_count(engine=engine)
+
     model_url = get_model_url()
     with open(model_url, 'rb') as f:
         model : LightFM = pickle.load(f)
     
     # print("추천 리스트 ")
     
-    all_item_ids=  np.array(model.item_embeddings.shape[0])
+    all_item_ids=  np.arange(model.item_embeddings.shape[0])
     # user_id는 1기준으로 입력받기 때문에, -1 해준다.
     recommendations = model.predict(user_ids=user_id, item_ids=all_item_ids)
     # 상위 size 개의 도서 id 반환한다. id는 0기준이므로 1씩 증가해서
@@ -37,19 +37,41 @@ def get_recommend_book_list(user_id : int, size : int ) -> List[dict] :
     
     length = len(recommendations)
 
+    choosed = set() 
+    
     top_end = length//2
     low_start = length//10*9
     top_recommend_list = recommendations[:top_end]
     low_recommend_list = recommendations[low_start:]
     # 상위에서 50% 중에서 7 개
+    # 데이터가 없으면 더 적게 선정 (사용자 데이터가 없는 경우)
+    
+    top_length = len(top_recommend_list)
+    top_count = min( top_length//10 * 7 , 7 ) 
+
+    indexes = random.sample(range(0, top_length), top_count)
+    for index in indexes :
+        choosed.add(top_recommend_list[index])
 
     # 하위에서 10% 중에서 1 개
+    low_length = len(low_recommend_list)
+    low_count = min(1, low_length//10)
 
-    # 랜덤하게 2 개 합쳐서 리턴하자.
+    indexes = random.sample(range(0, low_length), low_count)
+    for index in indexes : 
+        choosed.add(low_recommend_list[index])
+
+    # 남은 만큼은 랜덤하게 합쳐서 리턴하자.
+    random_count = size - top_count - low_count
+
+    numbers = random.sample(range(0, total_count+1), random_count)
+
+    for number in numbers : 
+        choosed.add(number)
 
 
+    response_data = get_book_list(engine,tuple(choosed))
 
-    response_data = get_book_list(engine,top_recommend_list)
-    
     return response_data
+    
 
