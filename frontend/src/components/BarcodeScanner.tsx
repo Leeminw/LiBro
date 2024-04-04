23644e8a8eb98e528836bdf619508565281dc8f3
유지웅 <yjw0835@naver.com>
2024-04-04 14:07:09 +0900

    Update BarcodeScanner.tsx

--- frontend/src/components/BarcodeScanner.tsx
+++ frontend/src/components/BarcodeScanner.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Camera, CameraType } from "react-camera-pro";
 import axios from "axios";
 import { LuSwitchCamera } from "react-icons/lu";
@@ -9,8 +9,17 @@ const BarcodeScannerComponent = ({
 }: {
   onScanned: (result: string) => void;
 }): React.ReactElement => {
+  const [numberOfCameras, setNumberOfCameras] = React.useState(0);
+  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+  const [deviceIdx, setDeviceIdx] = React.useState(0);
   const camera = useRef<CameraType>(null);
+  const [activeDeviceId, setActiveDeviceId] = React.useState<string | undefined>(undefined);
   React.useEffect(() => {
+    (async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter((i) => i.kind == "videoinput");
+      setDevices(videoDevices);
+    })();
     const interval = setInterval(async () => {
       try {
         const screenShot = camera.current?.takePhoto();
@@ -38,14 +47,18 @@ const BarcodeScannerComponent = ({
       <div
         className="absolute right-2 bottom-2 w-12 h-12 bg-black/60 rounded-full flex items-center justify-center z-10"
         onClick={() => {
-          camera.current?.switchCamera();
+          let idx = (deviceIdx+1)%numberOfCameras;
+          setDeviceIdx(idx);
+          setActiveDeviceId(devices[idx].deviceId);
         }}
       >
-        <LuSwitchCamera className="text-white w-6 h-6" />
+        <LuSwitchCamera className=" absolute text-white w-6 h-6 z-10" />
       </div>
       <Camera
         ref={camera}
+        numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
         facingMode="environment"
+        videoSourceDeviceId={activeDeviceId}
         errorMessages={{
           canvas: "지원되지 않는 기기입니다.",
           noCameraAccessible: "사용 가능한 카메라를 찾지 못했습니다.",
